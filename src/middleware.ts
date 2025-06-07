fea8871abe8a41b586313fc868ba35a3272fa5d2
Roshan Divekar <roshandiwekar@gmail.com>
2025-06-07 16:10:45 +0530

    check 3

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,11 @@ import { NextResponse } from "next/server";
 export default auth((req) => {
   if (!req.auth && req.nextUrl.pathname !== "/login") {
     const loginUrl = new URL("/login", req.nextUrl.origin);
-    return Response.redirect(loginUrl);
+    return NextResponse.redirect(loginUrl);
   }
   if(req.auth && (req.nextUrl.pathname === "/" || req.nextUrl.pathname === "/login")){
     const homeUrl = new URL("/home", req.nextUrl.origin);
-    return Response.redirect(homeUrl);
+    return NextResponse.redirect(homeUrl);
   }
   return NextResponse.next();
 })
